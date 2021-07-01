a0a74a54e58b9c18b3557963b7141cb0089ed793
Arter Tendean <arter.tendean.07@gmail.com>
2021-07-01 12:34:51 +0800

    initialize pyscord-storage

---
+++ src/pyscord_storage/__init__.py
@@ -0,0 +1,15 @@
+import json
+import requests
+
+
+def upload(filename, url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+    }
+    response = requests.post(
+        "https://api.animemoe.us/discord-storage/",
+        headers=headers,
+        data={"filename": filename, "url": url},
+    )
+
+    return {"status": requests.status_code, "data": json.dumps(response.text)}
