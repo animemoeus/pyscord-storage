9c1531c78c44bfb9bc26b8ac7c5898bd6ebe59e1
Arter Tendean <arter.tendean.07@gmail.com>
2022-06-29 16:23:07 +0800

    update

--- src/pyscord_storage/__init__.py
+++ src/pyscord_storage/__init__.py
@@ -2,11 +2,10 @@ import json
 import requests
 
 
-def upload(filename="", file="", custom_headers={}):
+def upload(filename="", file=""):
 
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-        "Referer": "pyscord-storage 0.1.3",
     }
 
     # upload file from URL
@@ -22,7 +21,7 @@ def upload(filename="", file="", custom_headers={}):
         response = requests.request(
             "POST",
             url,
-            headers=headers if custom_headers == {} else custom_headers,
+            headers=headers,
             data=payload,
         )
 
---
+++ src/test.py
@@ -0,0 +1,8 @@
+import pyscord_storage
+
+print(
+    pyscord_storage.upload(
+        "test.jpg",
+        "https://cdn.discordapp.com/attachments/858938620425404426/980409229393395722/waifu-animemoeus.jpg",
+    )
+)
