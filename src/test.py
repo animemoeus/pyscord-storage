5810b5e5bcabc023410d2cf1096738a63c945776
Arter Tendean <arter.tendean.07@gmail.com>
2022-11-03 18:13:55 +0800

    update test.py

--- src/test.py
+++ src/test.py
@@ -1,8 +1,13 @@
+import json
+
 import pyscord_storage
 
 print(
-    pyscord_storage.upload(
-        "test.jpg",
-        "https://cdn.discordapp.com/attachments/858938620425404426/980409229393395722/waifu-animemoeus.jpg",
+    json.dumps(
+        pyscord_storage.upload(
+            "test.jpg",
+            "https://cdn.discordapp.com/attachments/858938620425404426/980409229393395722/waifu-animemoeus.jpg",
+        ),
+        indent=1,
     )
 )
